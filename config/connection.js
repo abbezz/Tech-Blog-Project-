6545cd792c1b9e85ee69a010f8c666c046e66415
Abdollah Zamannoun <abbezz@live.com>
2022-07-10 23:21:35 +0100

    config file

--- config/connection.js
+++ config/connection.js
@@ -1,3 +1,11 @@
 require('dotenv').config();
 const Sequelize = require('sequelize');
+const DB_NAME = process.env.DB_NAME;
+const DB_USER = process.env.DB_USER;
+const DB_PASSWORD = process.env.DB_PASSWORD;
+const DB_HOST = process.env.DB_HOST;
+
+
+
+
 
