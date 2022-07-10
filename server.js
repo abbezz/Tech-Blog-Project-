40fa766ce61267c422522d44afdbb33f3666839a
Abdollah Zamannoun <abbezz@live.com>
2022-07-10 23:14:26 +0100

    defined my express app and my port

--- server.js
+++ server.js
@@ -8,4 +8,7 @@ const helpers = require('./utils/helpers.js');
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
+const app = express();
+const PORT = process.env.PORT || 3001;
+
 
