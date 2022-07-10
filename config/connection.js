0f881c9acd33509a91a0231c0585c9bb98925e93
Abdollah Zamannoun <abbezz@live.com>
2022-07-10 23:23:53 +0100

    connection.file done

--- config/connection.js
+++ config/connection.js
@@ -7,5 +7,19 @@ const DB_HOST = process.env.DB_HOST;
 
 
 
+const options = {
+  host: DB_HOST,
+  dialect: 'mysql',
+  port: 3306,
+  logging: false,
+};
+
+const connection = process.env.JAWSDB_URL
+? new Sequelize(JAWSDB_URL)
+: new Sequelize(DB_NAME, DB_USER,
+  DB_PASSWORD, options);
+
+  module.exports = connection;
+
 
 
